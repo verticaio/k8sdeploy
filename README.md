9cd9ecaa5b76af6a94c76b4a300435bb2beae8ff
Babak Mammadov <bmammadov@s-pro.io>
2022-02-23 04:42:55 +0400

    system,tools setup  and  helm chart added

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 
+
+
+Test App and DockerCompose and Nexus
 ### Docker
 docker build -t python-flask-docker .
 docker run --name python-flask-docker   -d -p 8080:8080 python-flask-docker  
 curl http://localhost:8080
 
-### Helm Chart
+steps 
+
+clone(first install git)
+
+
+
+improvement plan: 
+
+1. Ansible automate
+2. Harbor and Helm Chart Repo
+3. testing - e2e testing  
+4. makefile  
+5. sops encryption or get secrets from vault
+6. Delete old images from nexus 
+
---
+++ infra/setup/setup-req.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# Update System and Install required based packages
+sudo apt update 
+sudo apt install apt-transport-https ca-certificates curl software-properties-common git vim curl wget telnet  -y 
+
+# Install 
+sudo apt-get install openjdk-8-jdk -y 
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins -y 
+
+# Check Jenkins
+systemctl status jenkins
+
+# Change jenkins default port 9999 , edit jenkins port /etc/default/jenkins HTTP_PORT
+# sudo service jenkins restart
+# sudo systemctl status jenkins
+
+# Get Jenkins Password
+cat /var/lib/jenkins/secrets/initialAdminPassword 
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo systemctl status docker
+
+# Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Check Docker
+docker info 
+docker-compose --version
+
+# Enable sonatype nexus for image store
+cd /opt/k8sdeploy/infra/setup/tools
+
+
+# Install Nginx
+apt-get install nginx -y 
+nginx -v 
+systemctl status nginx
+
+
+# Copy Nginx config jenkins, nexus, custom app 
+
+# Install MiniKube
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+
+# Check Minikube
+minikube version
+
+# Install Kubectl 
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version -o yaml
+
+# Add user to start miikube with Docker driver
+useradd -s /bin/bash -d /home/docker -g docker -m -G sudo  docker 
+su - docker
+
+
+# Start minikube k8s cluster
+minikube start --addons=ingress --cpus=2 --cni=flannel --install-addons=true --kubernetes-version=stable --memory=6g --apiserver-ips=172.17.0.4
+#--apiserver-ips internal ip of vm    
+
+# Check minikube status
+minikube status
+kubectl cluster-info
+export KUBECONFIG=/home/docker/.kube/config 
+kubectl create deployment my-nginx --image=nginx
+kubectl get deployments.apps my-nginx
+kubectl get pods
+kubectl expose deployment my-nginx --name=my-nginx-svc --type=NodePort --port=80
+kubectl get svc my-nginx-svc
+minikube service my-nginx-svc --url
+curl http://192.168.49.2:30550
+
+
